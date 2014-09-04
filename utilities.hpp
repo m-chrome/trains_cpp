f5b6c887d92de4076232d88c7e225b90c14881e9
m-chrome <m.chrome@yandex.ru>
2014-09-04 16:37:11 +0400

    Create utilities.hpp

---
+++ utilities.hpp
@@ -0,0 +1,17 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstdbool>
+#include <cstring>
+
+using namespace std;
+
+namespace utilities {
+
+bool CheckFile(FILE *filename);
+//Метод проверки файла
+}
+
+#endif // UTILITIES_HPP
